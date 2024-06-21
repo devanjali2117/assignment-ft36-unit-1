669002cdfd4fb69b52f937d22fedd73bac005e6c
devanjali2117 <devanjalisingh20@gmil.com>
2024-06-21 22:08:58 +0530

    MYSQL-D1-L1

--- practics.py
+++ practics.py
@@ -1,6 +1,9 @@
-def add_number(a,b):
-    sum=a+b
-    print(sum)
-add_number()    
 
-    
+def solve(N,M,arr):
+
+    for j in range(M):
+        column_sum=0
+            for i in range(N):
+            if arr[i][j]%2 !=0:
+            column_sum+=arr[i][j]
+print(column_sum)        
